a39d4fec28b7e1179f480fd3740cc73a983f2f9e
Terence Lim <terencelimxp@gmail.com>
2019-02-16 04:41:42 -0500

    ibm-watson api fix

--- watson.py
+++ watson.py
@@ -1,28 +1,18 @@
 from watson_developer_cloud import NaturalLanguageUnderstandingV1
-import watson_developer_cloud.natural_language_understanding.features.v1 \
-    as Features
+from watson_developer_cloud.natural_language_understanding_v1 import Features, EntitiesOptions, KeywordsOptions, ConceptsOptions, CategoriesOptions
 
 API_KEY = 'XSEYce5PdhtoYy64ENJM5ioOcwjNLeMRx-XpZ3IEVUGq'
 
-natural_language_understanding = NaturalLanguageUnderstandingV1(
-    iam_apikey=API_KEY,
-    version="2017-02-27")
 
-response = natural_language_understanding.analyze(
-    text="IBM has one of the largest workforces in the world",
-    features=[
-        Features.Concepts(
-            # Concepts options
-            limit=50
-        ),
-        Features.Keywords(
-            # Keywords options
-            # sentiment=True,
-            # emotion=True,
-            limit=10
-        )
-    ]
-)
+def ibm_nlp(text_input):
+    natural_language_understanding = NaturalLanguageUnderstandingV1(
+        version="2017-02-27",
+        iam_apikey=API_KEY)
 
-print(response.text)
-
+    response = natural_language_understanding.analyze(
+        text=text_input,
+        features=Features(concepts=ConceptsOptions(),
+                          entities=EntitiesOptions(),
+                          keywords=KeywordsOptions(),
+                          categories=CategoriesOptions())).get_result()
+    return response
